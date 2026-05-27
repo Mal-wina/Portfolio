e555117fa7e24312005ac957916da4c607b36c15
Malwina <macza3623@oslomet.no>
2026-05-27 16:13:05 +0200

    created a navbar and html for about subpage

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,15 @@
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 import "./App.css"
+
 
 function App() {
   return (
     <main>
+      <Navbar />
       <Hero />
+      <About />
     </main>
   );
 }
---
+++ portfolio/src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <h2>MC.</h2>
+
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Skills</li>
+                <li>Contact</li>
+            </ul>
+
+            <button>Let's Connect</button>
+        </nav>
+    );
+}
+
+export default Navbar;
