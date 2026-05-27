8448672adbf2cbdfe39158d15ae41e67b540ac25
Malwina <macza3623@oslomet.no>
2026-05-27 15:36:52 +0200

    creating react

---
+++ portfolio/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Hi, I'm Malwina Czajka</h1>
+      <p>Aspiring Software Developer</p>
+
+      <button>View My Work</button>
+    </section>
+  );
+}
+
+export default Hero;
