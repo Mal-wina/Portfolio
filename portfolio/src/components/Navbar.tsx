49b4a8a9fe60383f040d413ead6e5a586ceddc83
Malwina <macza3623@oslomet.no>
2026-06-18 16:44:18 +0200

    fixed navbar to leave on mouse leave

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -2,31 +2,32 @@ import { useState } from "react";
 
 function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
+   
     return (
         <nav className="navbar">
             <h2>MC.</h2>
 
-            <div className="navbar">
             <button className="menu-button" 
                     onClick={() => setMenuOpen(!menuOpen)}>
                 {menuOpen ? "×" : "☰"}
             </button>
 
+            <div className="nav-menu" onMouseLeave={() => setMenuOpen(false)}>
             <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
                 <li>
-                    <a href="/#home">Home</a>
+                    <a href="/#home" onClick={() => setMenuOpen(false)}>Home</a>
                 </li>
                  <li>
-                    <a href="/#about">About</a>
+                    <a href="/#about" onClick={() => setMenuOpen(false)}>About</a>
                 </li>
                  <li>
-                    <a href="/#projects">Projects</a>
+                    <a href="/#projects" onClick={() => setMenuOpen(false)}>Projects</a>
                 </li>
                  <li>
-                    <a href="/#skills">Skills</a>
+                    <a href="/#skills" onClick={() => setMenuOpen(false)}>Skills</a>
                 </li>
                  <li>
-                    <a href="/#contact">Contact</a>
+                    <a href="/#contact" onClick={() => setMenuOpen(false)}>Contact</a>
                 </li>
             </ul>
            </div> 
